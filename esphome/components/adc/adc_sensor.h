5c7043610706e6b662629ed607752e21dee80b3e
sleiis <91851761+sleiis@users.noreply.github.com>
2025-06-28 03:38:18 +0200

    First test

--- esphome/components/adc/adc_sensor.h
+++ esphome/components/adc/adc_sensor.h
@@ -94,10 +94,15 @@ class ADCSensor : public sensor::Sensor, public PollingComponent, public voltage
 #endif  // USE_RP2040
 
 #ifdef USE_ESP32
+  // ✨ Member für Portierung auf ESP-IDF 5
+  adc1_channel_t channel1_{ADC1_CHANNEL_MAX};
+  adc2_channel_t channel2_{ADC2_CHANNEL_MAX};
+  adc_atten_t attenuation_{ADC_ATTEN_DB_0};
+  bool autorange_{false};
+
   adc_oneshot_unit_handle_t adc_handle_{nullptr};
   adc_cali_handle_t cali_handle_{nullptr};
   bool calibration_available_{false};
-#endif  // ESP_IDF_VERSION_MAJOR
 #endif  // USE_ESP32
 };
 
